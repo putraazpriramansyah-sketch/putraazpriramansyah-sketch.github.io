d37005c620b4f516ea29c1c385ce9e11a18f0cae
putraazpriramansyah-sketch <putraazpriramansyah@gmail.com>
2026-02-17 19:45:39 +0700

    Add files via upload
    Signed-off-by: putraazpriramansyah-sketch <putraazpriramansyah@gmail.com>

---
+++ script.js
@@ -0,0 +1,51 @@
+// Loader Control
+window.addEventListener('load', () => {
+    const loader = document.getElementById('loader');
+    setTimeout(() => {
+        loader.classList.add('fade-out');
+        setTimeout(() => { loader.style.display = 'none'; }, 800);
+    }, 2500);
+
+    document.body.style.overflowY = "auto";
+    // ---------------- //
+
+    setTimeout(() => {
+        loader.style.display = 'none';}, 800);}, 2500);        
+
+// Typing Effect
+const textElement = document.querySelector(".typing-text");
+const words = ["Beginner Developer", "Photographer", "Student"];
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function typeEffect() {
+    const currentWord = words[wordIndex];
+    if (isDeleting) {
+        textElement.textContent = currentWord.substring(0, charIndex - 1);
+        charIndex--;
+    } else {
+        textElement.textContent = currentWord.substring(0, charIndex + 1);
+        charIndex++;
+    }
+
+    if (!isDeleting && charIndex === currentWord.length) {
+        isDeleting = true;
+        setTimeout(typeEffect, 2000);
+    } else if (isDeleting && charIndex === 0) {
+        isDeleting = false;
+        wordIndex = (wordIndex + 1) % words.length;
+        setTimeout(typeEffect, 500);
+    } else {
+        setTimeout(typeEffect, isDeleting ? 100 : 200);
+    }
+}
+typeEffect();
+
+const menuIcon = document.querySelector('.menu-icon');
+const navLinks = document.querySelector('.navLinks');
+
+menuIcon.onclick = () => {
+
+navLinks.classList.toggle('active'); console.log;
+};
